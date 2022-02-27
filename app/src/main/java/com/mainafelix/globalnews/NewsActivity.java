81e846395d67915417d08361254f30ecc88dc460
felixmaina <karani.maina2010@gmail.com>
2022-02-27 19:28:48 +0300

    modified the data in the array

--- app/src/main/java/com/mainafelix/globalnews/NewsActivity.java
+++ app/src/main/java/com/mainafelix/globalnews/NewsActivity.java
@@ -20,28 +20,27 @@ public class NewsActivity extends AppCompatActivity {
     TextView newsTextView;
     @BindView(R.id.listView)
     ListView listView;
-    private String[]articles = new String[] {"Why is affordable housing plan favouring employed Kenyans?", "Cricket", "Covid -19 crisis", "Inter-country wars between Ukraine and russia", "NASA discovers a black holr in space", "How to handle poverty crisis", "Economy bulletin :" };
-    private String[] news = new String[] {" residents in Norway prefer Vegan Food as opposed to meat ", "what you should take on Breakfast", "Fish farming has been embraces in denmark",  "Coffee regarded as the no. 1 booster", "what is your preffered English Food" };
+    private String[]articles = new String[] {"Why is affordable housing plan favouring employed Kenyans?", "Covid -19 crisis", "Inter-country wars between Ukraine and russia", "NASA discovers a black hole in space", "How to handle poverty crisis", "Economy bulletin :" };
+    private String[]news = new String[] {"government hhas imposed a percentage monthly deduction to provide affordable housing  to citizens","due to the upsurge of covid19 citizens are advised to wear mask and sanitize regularly","war intensifies in areas around kiev ukraine,putin  declares that ukraine should not be regarded as a country ,", "Scientist are digging more on the causes o n the origin of  the black hole", "To solve crisis the government should  make free education available to all, people need skills",  "Gold prices lower due to the ongoing crisis between ukraine and  russia" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
         ButterKnife.bind(this);
-       globalNewsAdapter adapter  = new globalNewsAdapter (this, android.R.layout.simple_list_item_1,articles ,news);
+       globalNewsAdapter adapter  = new globalNewsAdapter (this, android.R.layout.simple_list_item_1,articles,news );
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String news = ((TextView)view).getText().toString();
-                //Log.v("Restaurants.activity","in the onItemClickListener");
-                Toast.makeText(NewsActivity.this,news,Toast.LENGTH_LONG).show();
+                String article = ((TextView)view).getText().toString();
+                Toast.makeText(NewsActivity.this,article,Toast.LENGTH_LONG).show();
                 Log.v(TAG,"in the on itemclick listener");
             }
         });
         Intent intent = getIntent();// recreating sn intent
         String news=intent.getStringExtra("news");//fetching the data fom the intent
-        newsTextView.setText("Here are all the news : " + news);//set text updates the text on LocationTextView to include the text+ defined location,
+        newsTextView.setText("Here are all the news :" + news);//set text updates the text on LocationTextView to include the text+ defined location,
 //        // Log.d(TAG, "In the onCreate method!");
        listView = (ListView) findViewById(R.id.listView);
 
--- app/src/main/java/com/mainafelix/globalnews/globalNewsAdapter.java
+++ app/src/main/java/com/mainafelix/globalnews/globalNewsAdapter.java
@@ -7,15 +7,30 @@ import androidx.annotation.NonNull;
 
 public class globalNewsAdapter extends ArrayAdapter {
     private Context context;
-    private String[] articles;
-    private String [] news;
-    public globalNewsAdapter( Context context, int resource, String[] articles, String[] news) {
+    private String[] Articles;
+    private String[] news;
+
+
+    public globalNewsAdapter( Context context, int resource, String[] articles, String[] news ) {
         super(context, resource);
         this.context = context;
-       this.news = news;
-        this.articles = articles;
+        Articles = articles;
         this.news = news;
+
+    }
+    @Override
+    public Object getItem(int position) {
+        String article = Articles[position];
+        String newsa = news[position];
+        return String.format("%s \nTrending News: %s", article,newsa);
     }
 
-
+    @Override
+    public int getCount() {
+        return Articles.length;
+    }
 }
+
+
+
+
