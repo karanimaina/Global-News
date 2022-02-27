c14d3c8a1e8dcb84a6e05dd90aaf9911fcd6aae2
felixmaina <karani.maina2010@gmail.com>
2022-02-27 18:11:31 +0300

    pass Array  Data into the array adapter

--- app/src/main/java/com/mainafelix/globalnews/NewsActivity.java
+++ app/src/main/java/com/mainafelix/globalnews/NewsActivity.java
@@ -2,10 +2,14 @@ package com.mainafelix.globalnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,15 +20,31 @@ public class NewsActivity extends AppCompatActivity {
     TextView newsTextView;
     @BindView(R.id.listView)
     ListView listView;
-    private String[]articles = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
-    private String[] news = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
+    private String[]articles = new String[] {"Why is affordable housing plan favouring employed Kenyans?", "Cricket", "Covid -19 crisis", "Inter-country wars between Ukraine and russia", "NASA discovers a black holr in space", "How to handle poverty crisis", "Economy bulletin :" };
+    private String[] news = new String[] {" residents in Norway prefer Vegan Food as opposed to meat ", "what you should take on Breakfast", "Fish farming has been embraces in denmark",  "Coffee regarded as the no. 1 booster", "what is your preffered English Food" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
         ButterKnife.bind(this);
-       globaArrayAdapter adapter  = new MyRestaurantsArrayAdapter (this, android.R.layout.simple_list_item_1,restaurants,cuisines);
+       globalNewsAdapter adapter  = new globalNewsAdapter (this, android.R.layout.simple_list_item_1,articles ,news);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String news = ((TextView)view).getText().toString();
+                //Log.v("Restaurants.activity","in the onItemClickListener");
+                Toast.makeText(NewsActivity.this,news,Toast.LENGTH_LONG).show();
+                Log.v(TAG,"in the on itemclick listener");
+            }
+        });
+        Intent intent = getIntent();// recreating sn intent
+        String news=intent.getStringExtra("news");//fetching the data fom the intent
+        newsTextView.setText("Here are all the news : " + news);//set text updates the text on LocationTextView to include the text+ defined location,
+//        // Log.d(TAG, "In the onCreate method!");
+       listView = (ListView) findViewById(R.id.listView);
+
+
+    }
 }
--- app/src/main/java/com/mainafelix/globalnews/globalNewsAdapter.java
+++ app/src/main/java/com/mainafelix/globalnews/globalNewsAdapter.java
@@ -1,10 +1,21 @@
 package com.mainafelix.globalnews;
 
 import android.content.Context;
+import android.widget.ArrayAdapter;
 
-public class globalNewsAdapter {
+import androidx.annotation.NonNull;
+
+public class globalNewsAdapter extends ArrayAdapter {
     private Context context;
-    private String[] Restaurants;
-    private String [] cuisines;
+    private String[] articles;
+    private String [] news;
+    public globalNewsAdapter( Context context, int resource, String[] articles, String[] news) {
+        super(context, resource);
+        this.context = context;
+       this.news = news;
+        this.articles = articles;
+        this.news = news;
+    }
+
 
 }
