7d386c0b61613b792881657660e66bda784ed75a
felixmaina <karani.maina2010@gmail.com>
2022-02-27 19:56:51 +0300

    add open the main activity

--- app/src/androidTest/java/com/mainafelix/globalnews/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/mainafelix/globalnews/MainActivityInstrumentationTest.java
@@ -1,4 +1,15 @@
 package com.mainafelix.globalnews;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityInstrumentationTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
 }
