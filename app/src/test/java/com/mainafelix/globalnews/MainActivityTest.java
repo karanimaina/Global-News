34ceadd0c4c74fc1695161b8aea7869351709b6b
felixmaina <karani.maina2010@gmail.com>
2022-02-27 19:35:03 +0300

    add config annotation in main Activity test

--- app/src/test/java/com/mainafelix/globalnews/MainActivityTest.java
+++ app/src/test/java/com/mainafelix/globalnews/MainActivityTest.java
@@ -1,4 +1,9 @@
 package com.mainafelix.globalnews;
 
+import android.os.Build;
+
+import org.robolectric.annotation.Config;
+
+@Config(sdk =  {Build.VERSION_CODES.N_MR1})
 public class MainActivityTest {
 }
