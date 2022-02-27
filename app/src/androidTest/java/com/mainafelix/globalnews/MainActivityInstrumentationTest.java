d07985ee110b2dbe28cf719e26721968c31b1a00
felixmaina <karani.maina2010@gmail.com>
2022-02-27 19:58:12 +0300

    validates E dit text

--- app/src/androidTest/java/com/mainafelix/globalnews/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/mainafelix/globalnews/MainActivityInstrumentationTest.java
@@ -1,9 +1,16 @@
 package com.mainafelix.globalnews;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
@@ -12,4 +19,8 @@ public class MainActivityInstrumentationTest {
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
-}
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("Nairobi"))
+                .check(matches(withText("Nairobi")));
+    }}
