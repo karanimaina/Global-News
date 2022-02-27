81c57ac6a512e6b41f5fec32e6bce4b8528d8679
felixmaina <karani.maina2010@gmail.com>
2022-02-27 16:07:35 +0300

    bind using a butterKnife

--- app/src/main/java/com/mainafelix/globalnews/MainActivity.java
+++ app/src/main/java/com/mainafelix/globalnews/MainActivity.java
@@ -2,6 +2,7 @@ package com.mainafelix.globalnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,3 +30,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         FindNewsButton.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v) {
+        if(v == FindNewsButton) {
+            String News = LocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, News.class);
+            intent.putExtra("News", News);
+            startActivity(intent);
+        }}
+}
+
