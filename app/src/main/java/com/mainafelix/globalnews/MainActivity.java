9888c6ce6b45802cbc46cd5e7852ed27b5dd82a4
felixmaina <karani.maina2010@gmail.com>
2022-02-27 10:32:48 +0300

    override the oncreate method

--- app/src/main/java/com/mainafelix/globalnews/MainActivity.java
+++ app/src/main/java/com/mainafelix/globalnews/MainActivity.java
@@ -9,13 +9,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = MainActivity.class.getSimpleName();
     //holds  our find restaurant  Button
     @BindView(R.id.findNewsButton)
-    Button FindRestaurantsButton;
+    Button FindNewsButton;
     @BindView(R.id.locationEditText)
     EditText LocationEditText;
     @BindView(R.id.appNameTextView)
     TextView ppNameTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);//runs all the default behaviours of an activity
+        setContentView(R.layout.activity_main);//tells the activity which layout to use which is activity_main
+        LocationEditText =(EditText) findViewById(R.id.locationEditText);
+        ButterKnife.bind(this);
+
+        FindNewsButton.setOnClickListener(this);
+    }
