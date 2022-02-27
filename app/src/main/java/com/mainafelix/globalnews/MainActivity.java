e0c8ea65e61d0f16790d3ea4dc844e5ab3871220
felixmaina <karani.maina2010@gmail.com>
2022-02-27 10:30:51 +0300

    bimd views using id

--- app/src/main/java/com/mainafelix/globalnews/MainActivity.java
+++ app/src/main/java/com/mainafelix/globalnews/MainActivity.java
@@ -3,12 +3,19 @@ package com.mainafelix.globalnews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    //holds  our find restaurant  Button
+    @BindView(R.id.findNewsButton)
+    Button FindRestaurantsButton;
+    @BindView(R.id.locationEditText)
+    EditText LocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView ppNameTextView;
