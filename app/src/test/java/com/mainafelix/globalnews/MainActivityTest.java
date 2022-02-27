22ec51f6b13e19d1d935bce7ff617223a017fd5f
felixmaina <karani.maina2010@gmail.com>
2022-02-27 19:50:20 +0300

    add test to validate text view content

--- app/src/test/java/com/mainafelix/globalnews/MainActivityTest.java
+++ app/src/test/java/com/mainafelix/globalnews/MainActivityTest.java
@@ -1,12 +1,19 @@
 package com.mainafelix.globalnews;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+import android.content.Intent;
 import android.os.Build;
+import android.widget.TextView;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
 @Config(sdk =  {Build.VERSION_CODES.N_MR1})
 @RunWith(RobolectricTestRunner.class)
@@ -19,4 +26,18 @@ public class MainActivityTest {
                 .resume()
                 .get();
     }
+    @Test
+    public void validateTextViewContent(){
+        TextView appNameTextView = activity.findViewById(R.id.appNameTextView);
+        assertEquals(true, "GLOBAL NEWS ".equals(appNameTextView.getText().toString()));
+    }
+//    @Test
+//    public void secondActivityStarted(){
+//        activity.findViewById(R.id.findNewsButton).performClick();
+//        Intent expectedIntent = new Intent(activity,NewsActivity.class);
+//        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+//        Intent actualIntent = shadowActivity.getNextStartedActivity();
+//        assertTrue(actualIntent.filterEquals(expectedIntent));
+//
+//    }
 }
