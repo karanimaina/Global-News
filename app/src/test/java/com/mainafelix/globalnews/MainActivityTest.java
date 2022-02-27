aff6588557740a2618cc2d503fa49b763e6782a2
felixmaina <karani.maina2010@gmail.com>
2022-02-27 19:38:50 +0300

    setup  a before annotation that creates an activity, and make it available to carry test

--- app/src/test/java/com/mainafelix/globalnews/MainActivityTest.java
+++ app/src/test/java/com/mainafelix/globalnews/MainActivityTest.java
@@ -2,11 +2,21 @@ package com.mainafelix.globalnews;
 
 import android.os.Build;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 @Config(sdk =  {Build.VERSION_CODES.N_MR1})
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
+    private  MainActivity activity;
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
 }
