911811e3d46fb2847cc87c64e481dab9634a5b96
felixmaina <karani.maina2010@gmail.com>
2022-02-27 17:35:16 +0300

    create an intent' git push

--- app/src/main/java/com/mainafelix/globalnews/NewsActivity.java
+++ app/src/main/java/com/mainafelix/globalnews/NewsActivity.java
@@ -3,12 +3,28 @@ package com.mainafelix.globalnews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NewsActivity extends AppCompatActivity {
+    public static final String TAG = NewsActivity.class.getSimpleName();
+    @BindView(R.id.newsTextView)
+    TextView newsTextView;
+    @BindView(R.id.listView)
+    ListView listView;
+    private String[]articles = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
+    private String[] news = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
-    }
+        ButterKnife.bind(this);
+       globaArrayAdapter adapter  = new MyRestaurantsArrayAdapter (this, android.R.layout.simple_list_item_1,restaurants,cuisines);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 }
---
+++ app/src/main/java/com/mainafelix/globalnews/globalNewsAdapter.java
@@ -0,0 +1,10 @@
+package com.mainafelix.globalnews;
+
+import android.content.Context;
+
+public class globalNewsAdapter {
+    private Context context;
+    private String[] Restaurants;
+    private String [] cuisines;
+
+}
