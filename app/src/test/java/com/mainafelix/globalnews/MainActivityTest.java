d208fd0dc3ee2dbd34d841efd9ce0c15461d1107
felixmaina <karani.maina2010@gmail.com>
2022-02-27 19:36:41 +0300

    add   a run with annotation  that imports the robolectric class

--- app/src/test/java/com/mainafelix/globalnews/MainActivityTest.java
+++ app/src/test/java/com/mainafelix/globalnews/MainActivityTest.java
@@ -2,8 +2,11 @@ package com.mainafelix.globalnews;
 
 import android.os.Build;
 
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 @Config(sdk =  {Build.VERSION_CODES.N_MR1})
+@RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 }
