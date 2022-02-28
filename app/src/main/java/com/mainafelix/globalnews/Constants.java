9cfff9da600b76c11f962c4e0191e8e3e7e61a11
felixmaina <karani.maina2010@gmail.com>
2022-02-28 18:44:48 +0300

    add  a build config field in the dependency

--- app/src/main/java/com/mainafelix/globalnews/Constants.java
+++ app/src/main/java/com/mainafelix/globalnews/Constants.java
@@ -1,5 +1,6 @@
 package com.mainafelix.globalnews;
 
 public class Constants {
-public static final String NEWS_BASE_URL="https://newsapi.org/"
+public static final String NEWS_BASE_URL="https://newsapi.org/";
+public static  final  String NEWS_API_KEY= BuildConfig.NEWS_API_KEY;
 }
