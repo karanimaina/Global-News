5465ceb66004dbe6a73265ca48568c64e04f28f5
felixmaina <karani.maina2010@gmail.com>
2022-02-28 23:32:59 +0300

    add News API client class

---
+++ app/src/main/java/com/mainafelix/globalnews/Categories.java
@@ -0,0 +1,38 @@
+package com.mainafelix.globalnews;
+
+public class Categories {
+        public String getCategory;
+        private String category;
+        private String categoryImageUrl;
+
+        public Categories(String category, String categoryImageUrl) {
+            this.category = category;
+            this.categoryImageUrl = categoryImageUrl;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public String getGetCategory() {
+            return getCategory;
+        }
+
+        public void setGetCategory(String getCategory) {
+            this.getCategory = getCategory;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+
+        public String getCategoryImageUrl() {
+            return categoryImageUrl;
+        }
+
+        public void setCategoryImageUrl(String categoryImageUrl) {
+            this.categoryImageUrl = categoryImageUrl;
+        }
+    }
+
+
