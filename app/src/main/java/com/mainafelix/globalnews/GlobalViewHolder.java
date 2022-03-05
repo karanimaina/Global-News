33336a3656425a0711947712e19316ed7a06863e
felixmaina <karani.maina2010@gmail.com>
2022-03-05 09:46:00 +0300

    override oncreate viewHolder adapter

---
+++ app/src/main/java/com/mainafelix/globalnews/GlobalViewHolder.java
@@ -0,0 +1,8 @@
+package com.mainafelix.globalnews;
+
+import android.view.View;
+
+public class GlobalViewHolder {
+    public GlobalViewHolder(View inflate) {
+    }
+}
--- app/src/main/java/com/mainafelix/globalnews/globalNewsAdapter.java
+++ app/src/main/java/com/mainafelix/globalnews/globalNewsAdapter.java
@@ -1,34 +1,40 @@
 package com.mainafelix.globalnews;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.mainafelix.globalnews.models.Article;
+
+import java.util.List;
+
 public class globalNewsAdapter extends RecyclerView.Adapter<GlobalViewHolder> {
     private Context context;
-    private String[] Articles;
+    private List<Article> heeadlines;
 
-
-
-    public globalNewsAdapter( Context context, int resource, String[] articles ) {
-        super(context, resource);
+    public globalNewsAdapter(Context context, List<Article> heeadlines) {
         this.context = context;
-        Articles = articles;
-
-
+        this.heeadlines = heeadlines;
     }
-    @Override
-    public Object getItem(int position) {
-        String article = Articles[position];
 
-        return String.format("%s \nTrending News: %s", article);
+    @NonNull
+    @Override
+    public GlobalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new GlobalViewHolder(LayoutInflater.from(context).inflate(R.layout.activity_news,parent,false));
     }
 
     @Override
-    public int getCount() {
-        return Articles.length;
+    public void onBindViewHolder(@NonNull GlobalViewHolder holder, int position) {
+    holder.t
+    }
+
+    @Override
+    public int getItemCount() {
+        return  heeadlines.size();
     }
 }
