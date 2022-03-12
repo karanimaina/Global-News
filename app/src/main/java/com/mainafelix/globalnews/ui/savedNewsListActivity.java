e9950503354cfda10038cbed4b8134825612d336
felixmaina <karani.maina2010@gmail.com>
2022-03-12 22:00:50 +0300

    add Saved list activity

---
+++ app/src/main/java/com/mainafelix/globalnews/ui/savedNewsListActivity.java
@@ -0,0 +1,16 @@
+package com.mainafelix.globalnews.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mainafelix.globalnews.R;
+
+public class savedNewsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
