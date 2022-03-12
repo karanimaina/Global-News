2978055139e3d85d48dbfa51aa7c740dc9108a1c
felixmaina <karani.maina2010@gmail.com>
2022-03-12 17:32:21 +0300

    add News pagerr Adapter

--- app/src/main/java/com/mainafelix/globalnews/NewsPagerAdapter.java
+++ app/src/main/java/com/mainafelix/globalnews/NewsPagerAdapter.java
@@ -1,4 +1,37 @@
 package com.mainafelix.globalnews;
 
-public class NewsPagerAdapter {
-}
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.mainafelix.globalnews.models.Article;
+import com.mainafelix.globalnews.ui.NewsFragment;
+
+import java.util.List;
+
+public class NewsPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Article> news;
+
+        public NewsPagerAdapter(@NonNull FragmentManager fm, int behavior, List<Article> news1) {
+            super(fm, behavior);
+            news = news1;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return NewsFragment.newInstance(news.get(position));
+        }
+
+        @Override
+        public int getCount() {
+            return news.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return news.get(position).getTitle();
+        }
+    }
+
